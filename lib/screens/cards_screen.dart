72a990a370f34a0879bc1cb52cc0cdb300354e59
Richar Cangui <richarsantiagocangui96@gmail.com>
2024-05-28 18:56:26 -0500

    chore: Refactor CardsScreen layout and styling
    This commit refactors the layout and styling of the CardsScreen widget in cards_screen.dart. It improves the readability and organization of the code by removing unnecessary comments and formatting inconsistencies. The changes include fixing indentation, removing trailing whitespace, and ensuring consistent use of camel case for variable names. This refactor enhances the maintainability and clarity of the codebase.

--- lib/screens/cards_screen.dart
+++ lib/screens/cards_screen.dart
@@ -19,14 +19,14 @@ class CardsScreen extends StatelessWidget {
           /// 2. EdgeInsets.symmetric -> padding horizontal y vertical
           /// 3. EdgeInsets.only -> padding solo a un lugar en específico
           padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-          child:  Column(
+          child: Column(
             /// Propiedad que define el tamaño de la columna
             /// MainAxisSize.max -> defecto -> columna ocupe todo el espacio disponible
             /// MainAxisSize.min -> ocupa el tamaño el contenido
             mainAxisSize: MainAxisSize.min,
             children: [
               // https://api.flutter.dev/flutter/material/Card-class.html
-              // Generamos el icono | textos 
+              // Generamos el icono | textos
               const Row(
                 children: [
                   /// Construir el icono
@@ -45,35 +45,36 @@ class CardsScreen extends StatelessWidget {
                     child: Column(
                       /// Define el tipo de alineación en el eje opuesto (horizontal)
                       /// CrossAxisAlignment.center - default
-                      /// CrossAxisAlignment.start 
+                      /// CrossAxisAlignment.start
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         /// Title
                         /// Actualizar los estilos
                         /// style: TextStyle( ... propiedades ... ) /// test las propiedades
                         Text('Titulo de la tarjeta'),
+
                         /// Body
-                        Text('Cuerpo de la tarjeta Cuerpo de la tarjeta Cuerpo de la tarjeta'),
+                        Text(
+                            'Cuerpo de la tarjeta Cuerpo de la tarjeta Cuerpo de la tarjeta'),
                       ],
                     ),
                   ),
                 ],
               ),
 
-              // Generar los botones 
+              // Generar los botones
               // btn1 | btn2
               Row(
                 /// mainAxisAlignment -> Define la alineación del eje principal - horizontal
-                /// MainAxisAlignment.start -> default 
+                /// MainAxisAlignment.start -> default
                 /// MainAxisAlignment.end -> envia al final
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   /// Btn 1
-                  TextButton(onPressed: (){}, child: Text("Acción 1")),
+                  TextButton(onPressed: () {}, child: Text("Acción 1")),
 
                   /// Btn 2
-                  TextButton(onPressed: (){}, child: Text("Acción 2")),
-
+                  TextButton(onPressed: () {}, child: Text("Acción 2")),
                 ],
               ),
             ],
