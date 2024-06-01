0277c2cf4aa6dd84ecc802b4a6b7c7fdb0e3a2ec
Richar Cangui <richarsantiagocangui96@gmail.com>
2024-05-31 19:57:35 -0500

    ```text feat: Add InfinityScrollScreen, SlidersScreen, and PhotoAlbumScreen to routes
    This commit adds the `InfinityScrollScreen`, `SlidersScreen`, and `PhotoAlbumScreen` widgets to the project and updates the routes in `routes.dart` to include the new screens. These new features enhance the functionality of the app by providing users with the ability to scroll through a list of items infinitely, interact with sliders to control image scale and rotation, and view and interact with photo albums.

--- lib/screens/infinity_scroll_screen.dart
+++ lib/screens/infinity_scroll_screen.dart
@@ -13,7 +13,7 @@ class InfinityScrollScreen extends StatefulWidget {
 
 class _InfinityScrollScreenState extends State<InfinityScrollScreen> {
   /// Creando el objeto de la clase Services
-  /// NUNCA LO HAGAN -> prod 
+  /// NUNCA LO HAGAN -> prod
   final imagesObj = Services();
 
   /// Simpre me la creo como nula
@@ -22,7 +22,8 @@ class _InfinityScrollScreenState extends State<InfinityScrollScreen> {
   List<String>? images;
 
   /// Variable 1 que va a ser la imagen inicial
-  final int initialImage = 0;
+  /// Xq esta variable va a ser nuestra guía para consultar nuevas imagenes
+  int initialImage = 0;
 
   /// Variable 2 que va a ser el amount
   final amount = 10;
@@ -33,6 +34,10 @@ class _InfinityScrollScreenState extends State<InfinityScrollScreen> {
   // final scrollController = ScrollController();
   /// Forma 2
   late ScrollController scrollController;
+
+  /// Bandera que va a controlar el fetch de los servicios
+  /// Ademas controlar que no se hagan multiples peticiones en el scroll
+  bool isLoading = false;
 
   /// Hemos visto Statefull
   /// setState((){})
@@ -55,6 +60,44 @@ class _InfinityScrollScreenState extends State<InfinityScrollScreen> {
     setState(() {});
   }
 
+  /// Crear una nueva función paginado
+  Future<void> paginado() async {
+    ///1. obtener el valor de la página final
+    ///2. realizar una consulta al servicio
+    ///3. agrupar las listas
+    ///4. actualizar la interfaz
+
+    /// 1.
+    /// 0 ------- 9
+    /// 0+9 = 9
+    final finalPage = initialImage + amount;
+    print("FinalPage");
+    print(finalPage);
+
+    /// mutar nuestra variable initialImage
+    /// 0 > 9
+    /// 9 > 19
+    /// 19 > 29
+    initialImage = finalPage + 1;
+    print("InitialPage");
+    print(initialImage);
+
+    /// 2.
+    isLoading = true;
+    final newImages = await imagesObj.getImages(initialImage, amount);
+    isLoading = false;
+
+    ///3.
+    ///addAll es para agregar todos los elementos de una lista a otra lista
+    /// A = [1,2,3]
+    /// B = [4,5,6]
+    /// A =  A.addAll(B) = [1,2,3,4,5,6]
+    images?.addAll(newImages);
+
+    ///4.
+    setState(() {});
+  }
+
   /// dispose -> se va a detonar al destruir la pantalla - cerrarla
   @override
   void dispose() {
@@ -62,6 +105,14 @@ class _InfinityScrollScreenState extends State<InfinityScrollScreen> {
     scrollController.dispose();
     super.dispose();
   }
+
+  /// 1. NotificationListener -> Widget que escucha un cambio de un scroll
+  ///   --- Necesita envolver a un widget que sea de tipo Scroll
+  ///   --- ListView - .builder .separated
+  ///   --- GridView - .buider .count
+  ///   --- SingleChildScrollView
+  ///
+  /// 2. Usando los Listeners que puede llamarse desde un ScrollController
 
   @override
   Widget build(BuildContext context) {
@@ -72,14 +123,37 @@ class _InfinityScrollScreenState extends State<InfinityScrollScreen> {
           TextButton(
               onPressed: () {
                 /// animateTo nos permite mover el scroll
-                // scrollController.animateTo(offset, duration: duration, curve: curve)
+                scrollController.animateTo(
+                    scrollController.position.minScrollExtent,
+                    duration: const Duration(milliseconds: 100),
+                    curve: Curves.bounceIn);
 
                 // se salta a un offset en específico
                 // scrollController.position.maxScrollExtent -> saltar hasta el fondo de la página
-                scrollController
-                    .jumpTo(scrollController.position.maxScrollExtent);
+                // scrollController
+                //     .jumpTo(scrollController.position.maxScrollExtent);
               },
-              child: const Text("Ir al fondo")),
+              child: const Text("Arriba")),
+          TextButton(
+              onPressed: () {
+                /// animateTo nos permite mover el scroll
+                scrollController.animateTo(
+                  /// Posicionamiento
+                  scrollController.position.maxScrollExtent,
+
+                  /// Tiempo Animación -> 250 - 400 ms
+                  duration: const Duration(milliseconds: 100),
+
+                  /// Animación
+                  curve: Curves.bounceIn,
+                );
+
+                // se salta a un offset en específico
+                // scrollController.position.maxScrollExtent -> saltar hasta el fondo de la página
+                // scrollController
+                //     .jumpTo(scrollController.position.maxScrollExtent);
+              },
+              child: const Text("Abajo")),
         ],
       ),
       // Esto no es una buena práctica
@@ -100,26 +174,69 @@ class _InfinityScrollScreenState extends State<InfinityScrollScreen> {
             return const Center(child: Text("Está vacío"));
           }
 
-          return ListView.builder(
-            controller: scrollController,
+          ///    Clase <Tipo de dato>
+          ///    Personas<String>
+          /// Dentro de la clase existe un parámetro dinámico
+          /// T variable
+          /// String variable
+          /// Personas<double>
+          /// T variable
+          /// double variable
+          return NotificationListener<ScrollNotification>(
+            onNotification: (notification) {
+              // print(notification.metrics.pixels);
+              // print(notification.metrics.atEdge);
 
-            /// Retornar el widget a renderizar
-            itemBuilder: (context, index) {
-              return Card(
-                child: Image.network(
-                  images![index],
-                  height: 300,
+              /// Hacer una condición que me indique
+              /// 1. Que el scroll ya llegó al fondo o el final
+              /// 2. Que me indique cuando esta por llegar al fondo - 300px antes
 
-                  /// fit todas las imagenes
-                  /// define la inscripción de una imagen
-                  /// BoxFit
-                  /// BoxFit.cover -> expande la imagen -> mucho cuidado con las dimensiones de las imagenes
-                  /// xq se puede pixelear
-                  fit: BoxFit.fitWidth,
-                ),
-              );
+              /// 1.
+              if (notification.metrics.atEdge &&
+                  notification.metrics.pixels > 0 &&
+                  !isLoading) {
+                print("El scroll ha llegado al final");
+                paginado();
+              }
+
+              /// Posición final del scroll
+              /// notification.metrics.maxScrollExtent - 1000 , 2000, 3000
+              /// Dependiendo de cuantos elementos tengamos en el scroll
+              ///  Pixeles
+              ///   1  -> minScrollExtent
+              ///   2
+              ///   3
+              ///   4  ---- Límite
+              ///   5
+              ///   6   -> maxScrollExtent
+              if (notification.metrics.pixels >
+                  (notification.metrics.maxScrollExtent - 500)) {
+                print("Está llegando al fondo");
+              }
+
+              return true;
             },
-            itemCount: images!.length,
+            child: ListView.builder(
+              controller: scrollController,
+
+              /// Retornar el widget a renderizar
+              itemBuilder: (context, index) {
+                return Card(
+                  child: Image.network(
+                    images![index],
+                    height: 300,
+
+                    /// fit todas las imagenes
+                    /// define la inscripción de una imagen
+                    /// BoxFit
+                    /// BoxFit.cover -> expande la imagen -> mucho cuidado con las dimensiones de las imagenes
+                    /// xq se puede pixelear
+                    fit: BoxFit.fitWidth,
+                  ),
+                );
+              },
+              itemCount: images!.length,
+            ),
           );
         },
       ),
